f1a65d462ad8f404209f9f48ddeda50229bd5e03
matthewmaloneydev@gmail.com <matthew@Ruobings-MacBook-Pro.local>
2023-08-11 15:43:08 -0400

    ViewDirector refectored to provide therapist context

---
+++ pages/clients.js
@@ -0,0 +1,23 @@
+import { useContext, useEffect, useState } from 'react';
+import TherapistContext from '../utils/context/therapistContext';
+import { getClientsByTherapistId } from '../utils/databaseCalls/clientData';
+import ClientDetailsCard from '../components/cards/clientDetails';
+
+export default function ClientsPage() {
+  const [therapistClients, setTherapistClients] = useState([]);
+  const { therapist } = useContext(TherapistContext);
+
+  useEffect(() => {
+    console.warn('therapist', therapist);
+    getClientsByTherapistId(therapist.therapistId).then(setTherapistClients);
+  }, [therapist.therapistId]);
+
+  return (
+    <>
+      {therapistClients &&
+        therapistClients.map((client) => (
+          <ClientDetailsCard clientObj={client} />
+        ))}
+    </>
+  );
+}
