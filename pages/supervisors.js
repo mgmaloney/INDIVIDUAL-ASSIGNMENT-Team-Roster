637390b59256b9efc0f9e30c10e2258b6c42b9b9
matthewmaloneydev@gmail.com <matthew@Ruobings-MacBook-Pro.local>
2023-09-09 13:46:21 -0500

    admin supervisor view working

---
+++ pages/supervisors.js
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react';
+import { getAllSupervisors } from '../utils/databaseCalls/supervisorData';
+import TherapistCard from '../components/cards/therapistCard';
+
+export default function Supervisors() {
+  const [supervisors, setSupervisors] = useState([]);
+  const [showingSupervisors, setShowingSupervisors] = useState([]);
+
+  useEffect(() => {
+    getAllSupervisors().then(setSupervisors);
+  }, []);
+
+  useEffect(() => {
+    const activeSupervisors = [];
+    supervisors.forEach((supervisor) => {
+      if (supervisor.active) {
+        activeSupervisors.push(supervisor);
+      }
+    });
+    setShowingSupervisors(activeSupervisors);
+  }, [supervisors]);
+
+  const onSupervisorUpdate = () => {
+    getAllSupervisors().then(setSupervisors);
+  };
+
+  const handleActiveSort = (e) => {
+    const updatedShowingSupervisors = [];
+    if (e.target.value === 'active') {
+      supervisors.forEach((client) => {
+        if (client.active) {
+          updatedShowingSupervisors.push(client);
+        }
+      });
+      setShowingSupervisors(updatedShowingSupervisors);
+    } else if (e.target.value === 'inactive') {
+      supervisors.forEach((client) => {
+        if (client.active === false) {
+          updatedShowingSupervisors.push(client);
+        }
+      });
+      setShowingSupervisors(updatedShowingSupervisors);
+    } else if (e.target.value === 'all') {
+      setShowingSupervisors(supervisors);
+    }
+  };
+
+  const handleSearch = (e) => {
+    const filteredSupervisors = [];
+    supervisors.forEach((supervisor) => {
+      if (
+        supervisor.firstName.toLowerCase().includes(e.target.value) ||
+        supervisor.lastName.toLowerCase().includes(e.target.value)
+      ) {
+        filteredSupervisors.push(supervisor);
+      }
+    });
+    setShowingSupervisors(filteredSupervisors);
+  };
+
+  return (
+    <>
+      <h1 className="list-header">Supervisors: </h1>
+      <select className="active-sort" onChange={handleActiveSort}>
+        <option value="active" defaultValue="active">
+          Active Supervisors
+        </option>
+        <option value="inactive">Inactive Supervisors</option>
+        <option value="all">All Supervisors</option>
+      </select>
+      <input
+        type="search"
+        placeholder="Search..."
+        className="search-clients"
+        onChange={handleSearch}
+      />
+      <div className="therapists">
+        {showingSupervisors &&
+          showingSupervisors.map((supervisor) => (
+            <TherapistCard
+              therapistObj={supervisor}
+              onTherapistUpdate={onSupervisorUpdate}
+            />
+          ))}
+      </div>
+    </>
+  );
+}
